729c30d0854dab6b99074b74b36c77e6784149f7
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2021-11-30 23:35:39 +0300

    Added func for f_ext.

--- zadanie 7/Task4.cpp
+++ zadanie 7/Task4.cpp
@@ -5,18 +5,9 @@
 
 using namespace std;
 
-int main() {
-    char string[80];
-    char f_ext[80];
-    int i;
-    
-    cout << "Write your file name:" << endl;
-    gets(string);
+void file_extension_rename(char *string, char *f_ext) {
 
-    cout << "Write your need file extension with point (f.e: .php):" << endl;
-    gets(f_ext);
-
-    i = 0;
+    int i = 0;
     int index_point = 0;
     while (string[i] != '\0') {
         if (string[i] == '.') {index_point = i;}
@@ -25,6 +16,21 @@ int main() {
     if (index_point != 0)
         strcpy(string+index_point,f_ext);
     else strcat(string,f_ext);
+
+}
+
+int main() {
+    char string[80];
+    char f_ext[80];
+    int i;
+
+    cout << "Write your file name:" << endl;
+    gets(string);
+
+    cout << "Write your need file extension with point (f.e: .php):" << endl;
+    gets(f_ext);
+
+    file_extension_rename(string,f_ext);
 
     cout << "Result:" << endl;
     cout << string;
