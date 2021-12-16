894161a95425de311fc671d5b60aca6d2b2fe861
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2021-12-17 01:33:21 +0300

    m_funcs moved to m_funcs.cpp

---
+++ zadanie 8/m_funcs.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "m_funcs.h"
+
+
+void initialization(int **array, const int size_str, const int size_col);
+void print_array(int **array, const int size_str, const int size_col);
