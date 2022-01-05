b8433e8a3f85645cdc013fce8509ff09970d5faa
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2022-01-05 21:29:54 +0300

    Delete zadanie 8 directory

--- zadanie 8/m_funcs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "m_funcs.cpp"
-
-
-int random();
-void initialization(int **array, const int size_str, const int size_col);
-void print_array(int **array, const int size_str, const int size_col);
-void del_array(int** array, int str, int col);
